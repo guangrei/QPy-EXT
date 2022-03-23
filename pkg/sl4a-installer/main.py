c2742a0b7ea1302662844ea17ef17531017ea116
Am K <myawn@pm.me>
2022-03-23 20:27:13 +0700

    add sl4a-installer

---
+++ pkg/sl4a-installer/main.py
@@ -0,0 +1,23 @@
+# -*-coding:utf8;-*-
+import os
+import sys
+"""
+QPy CMD extension
+name: sl4a-installer
+description: Sl4a module installer.
+version: v0.1
+author: guangrei
+require: ""
+"""
+argv = sys.argv # argv
+path = os.environ["EXT_DIR"] # your extension path
+if __name__ == "__main__":
+    try:
+        import androidhelper
+        print("you already have sl4a module!")
+    except ImportError:
+        print("installing sl4a module..")
+        py = sys.executable
+        com = "{0} {1} install".format(py, path+"/setup.py")
+        os.system(com)
+        print("done!")
---
+++ pkg/sl4a-installer/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='androidhelper',
+    version="v0.01",
+    description='',
+    long_description="",
+    author="guangrei",    author_email='myawn{[AT]}pm.me',
+    py_modules=['androidhelper'],
+    license='MIT',
+    platforms='any',
+)
