f394ab1d3b3b19588a8779802cce09dba1a5607e
Am K <myawn@pm.me>
2022-10-31 18:20:23 +0700

    update

---
+++ pkg/modulebk/main.py
@@ -0,0 +1,14 @@
+# -*-coding:utf8;-*-
+import myext as ext
+
+"""
+QPy CMD extension
+name: modulebk
+description: module backuper
+version: v0.0.1
+author: guangrei
+require: ""
+"""
+
+if __name__ == "__main__":
+    ext.main()
